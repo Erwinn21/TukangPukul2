98904dc84829546cbaaf9d5415ef350e21fc2c61
WillyLayanto <layanto86@gmail.com>
2021-06-10 23:33:28 +0700

    information widget

--- app/src/main/java/com/example/myapplication/WidgetPref.kt
+++ app/src/main/java/com/example/myapplication/WidgetPref.kt
@@ -1,15 +1,14 @@
 package com.example.myapplication
 
 import android.content.Context
-
+//deklarasi key
 val PREF_NAME = "MyPref"
 val KEYS_WIDGET_ID = "widgetIds"
 class WidgetPref (context: Context) {
+    //buat shared preferences untuk menyimpan semua id widget
     val pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
     fun setId(ids : MutableSet<String>){
-        val editor = pref.edit()
-        editor.putStringSet(KEYS_WIDGET_ID, ids)
-        editor.apply()
+        pref.edit().putStringSet(KEYS_WIDGET_ID, ids).apply()
     }
     fun getId() = pref.getStringSet(KEYS_WIDGET_ID, hashSetOf())
 }
