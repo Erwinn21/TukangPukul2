446db82766ce52d755b38da5837bd410f3071825
Erwinn21 <erwiinn21@gmail.com>
2021-06-10 23:32:23 +0700

    a

--- app/src/main/java/com/example/myapplication/InformationWidget.kt
+++ app/src/main/java/com/example/myapplication/InformationWidget.kt
@@ -28,6 +28,7 @@ class InformationWidget : AppWidgetProvider() {
 
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
+            ids?.add(appWidgetId.toString())
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
         if(ids != null)
--- app/src/main/java/com/example/myapplication/WidgetPref.kt
+++ app/src/main/java/com/example/myapplication/WidgetPref.kt
@@ -6,7 +6,7 @@ val PREF_NAME = "MyPref"
 val KEYS_WIDGET_ID = "widgetIds"
 class WidgetPref (context: Context) {
     val pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-    fun setId(ids : MutableSet<String>){
+    fun setId(ids : MutableSet<String>) {
         val editor = pref.edit()
         editor.putStringSet(KEYS_WIDGET_ID, ids)
         editor.apply()
